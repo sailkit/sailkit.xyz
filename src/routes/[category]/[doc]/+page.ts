48ffd9a7bae4533906d23cde90a47580f58b3f4b
Noam Shemesh <noams@hey.com>
2025-06-18 17:45:28 +0300

    🚀 switch to cloudflare

--- src/routes/[category]/[doc]/+page.ts
+++ src/routes/[category]/[doc]/+page.ts
@@ -1,6 +1,21 @@
 import { error } from '@sveltejs/kit';
+import type { EntryGenerator, PageLoad } from './$types';
 
-export async function load({ params }) {
+export const entries: EntryGenerator = () => {
+  const modules = import.meta.glob('/src/lib/docs/**/*.md');
+
+  return Object.keys(modules).map((path) => {
+    const parts = path.split('/');
+    const category = parts[4];
+    const doc = parts[5].replace('.md', '');
+
+    return { category, doc };
+  });
+};
+
+export const prerender = true;
+
+export const load: PageLoad = async ({ params }) => {
   try {
     const doc = await import(`$lib/docs/${params.category}/${params.doc}.md`);
 
@@ -15,4 +30,4 @@ export async function load({ params }) {
     console.log(e);
     throw error(404, 'Oops, the page you are looking for does not exist.');
   }
-}
+};
